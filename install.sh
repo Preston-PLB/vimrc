b85ce2ff7f46a81d69ded9fc8086e7973f501936
Preston Baxter <pbaxter@pnmac-7f9ce0.pnmac.com>
2021-06-15 22:28:21 -0500

    added .zshrc (again for real)

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ shopt -s dotglob
 for FILE in *
 do 
 	if [[ $FILE =~ ^[.].* &&  "$FILE" != ".git" ]]; then 
-		echo "Linked $(realpath $FILE) To  $HOME/$FILE"
+		echo "Linked $PWD/$FILE To  $HOME/$FILE"
 		
 		if [[ "$1" == "-f" && -f "$HOME/$FILE" ]]; then
 			rm $HOME/$FILE
 		fi
 		
-		ln -s $(realpath $FILE) $HOME/$FILE
+		ln -s $PWD/$FILE $HOME/$FILE
 	fi
 done
